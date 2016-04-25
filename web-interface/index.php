a341dc62458c7a41051aa66ee46eb031b3fe049a
z-gora <domdlapsa@gmail.com>
2016-04-25 16:19:49 +0200

    Basic filesystem
    Created some template files to work on.

---
+++ web-interface/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WYD Dictionary</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="">
+        <script type="text/javascript" src="jquery-1.12.2.min.js"></script>
+        <script type="text/javascript" src="main.js"></script>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.blue-amber.min.css" />
+        <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
+        <link rel="stylesheet" href="main.css" />
+    </head>
+    <body>
+        
+    </body>
+</html>
