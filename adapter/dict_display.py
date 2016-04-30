ddf856b41e1453e893de74789bc4de57d7c908ac
Ddedalus <h.beres@hotmail.com>
2016-04-30 22:08:49 +0200

    First commit, trying to adapt code downloaded in recipe.py

--- adapter/dict_display.py
+++ adapter/dict_display.py
@@ -1,68 +1,136 @@
 # -*- coding: utf-8 -*-
 
+import cStringIO, operator
 
 def display_dict(dicts, columns = ('id', 'base'), header=True):
+	'''
+
+	:param dicts: data in dicts to be printed
+	:param columns: column names to be printed
+	:param header: if True - print header, if "Only" - print only header
+	:return: string to be displayed or error number
+	'''
 
 	if isinstance(dicts, list):
 		print 'ok'
 	elif isinstance(dicts, dict):
 		dicts = [dicts]
 	else:
-		print "Unrecognised type of dic! " + str(type(dicts))
-		return 1
-
-	def create_header():
-		h = ''
-		for col in columns:
-			h += str(col) + '\t'
-			if col == 'mono':
-				h += '\t\t\t'
-			elif col == 'author':
-				h += '\t'
-		return h[:-1] + '\n'
+		return "1: Unrecognised type of dict! " + str(type(dicts))
 
 
-	if header  == False:
-		pass
-	elif header == "Only":
-		print create_header()
-		return 0
-	elif header == True:
-		print create_header()
-	else:
-		print "Unknown header value: " + str(header)
-		return 2
+
+	for row in columns:
+		try:
+			dicts[0][row]
+		except KeyError:
+			del columns[row]
+			print "No " + row + " found in given dict!"
 
 
-	for word in dicts:
-		s = u''
-		for col in columns:
-			add = u''
+	#make 2D table from dicts
+	tab = [[ word[val] for val in columns] for word in dicts]
 
-			try:
-				add += word[col][:20] +'\t'
+	return indent(dicts, hasHeader=True)
 
-			except KeyError:
-				add += "null\t"
 
-			except TypeError:
-				add += unicode(word[col]) + '\t'
+def indent(rows, hasHeader=False, headerChar='-', delim=' | ', justify='left',
+           separateRows=False, prefix='', postfix='', wrapfunc=lambda x: x):
+	"""Indents a table by column.
+	   - rows: A sequence of sequences of items, one sequence per row.
+	   - hasHeader: True if the first row consists of the columns' names.
+	   - headerChar: Character to be used for the row separator line
+		 (if hasHeader==True or separateRows==True).
+	   - delim: The column delimiter.
+	   - justify: Determines how are data justified in their column.
+		 Valid values are 'left','right' and 'center'.
+	   - separateRows: True if rows are to be separated by a line
+		 of 'headerChar's.
+	   - prefix: A string prepended to each printed row.
+	   - postfix: A string appended to each printed row.
+	   - wrapfunc: A function f(text) for wrapping text; each element in
+		 the table is first wrapped by this function."""
 
-			if col == u'mono':
-				if len(add) < 6:
-					add += "\t\t\t"
-					#print "Prelonging"
-				elif len(add) < 10:
-					add += "\t\t"
-				elif len(add) < 14:
-					add += "\t"
-					#print "Prelonging"
-			elif col == u'author' and len(add) < 6:
-				add += "\t"
+	# closure for breaking logical rows to physical, using wrapfunc
+	def rowWrapper(row):
+		newRows = [wrapfunc(item).split('\n') for item in row]
+		return [[substr or '' for substr in item] for item in map(None, *newRows)]
 
-			s += add
+	# break each logical row into one or more physical ones
+	logicalRows = [rowWrapper(row) for row in rows]
+	# columns of physical rows
+	columns = map(None, *reduce(operator.add, logicalRows))
+	# get the maximum of each column by the string length of its items
+	maxWidths = [max([len(unicode(item)) for item in column]) for column in columns]
+	rowSeparator = headerChar * (len(prefix) + len(postfix) + sum(maxWidths) + \
+	                             len(delim) * (len(maxWidths) - 1))
+	# select the appropriate justify method
+	justify = {'center': str.center, 'right': str.rjust, 'left': str.ljust}[justify.lower()]
+	output = cStringIO.StringIO()
+	if separateRows: print >> output, rowSeparator
+	for physicalRows in logicalRows:
+		for row in physicalRows:
+			print >> output, \
+				prefix \
+				+ delim.join(
+					[justify(unicode(item), width) for (item, width) in zip(row, maxWidths)]) \
+				+ postfix
+		if separateRows or hasHeader: print >> output, rowSeparator; hasHeader = False
+	return output.getvalue()
 
-		print s[:-1]
-	print ''	#empty line
 
-	return 0
+# written by Mike Brown
+# http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/148061
+def wrap_onspace(text, width):
+	"""
+	A word-wrap function that preserves existing line breaks
+	and most spaces in the text. Expects that existing line
+	breaks are posix newlines (\n).
+	"""
+	return reduce(lambda line, word, width=width: '%s%s%s' %
+	                                              (line,
+	                                               ' \n'[(len(line[line.rfind('\n') + 1:])
+	                                                      + len(word.split('\n', 1)[0]
+	                                                            ) >= width)],
+	                                               word),
+	              text.split(' ')
+	              )
+
+
+import re
+
+
+def wrap_onspace_strict(text, width):
+	"""Similar to wrap_onspace, but enforces the width constraint:
+	   words longer than width are split."""
+	wordregex = re.compile(r'\S{' + unicode(width) + r',}')
+	return wrap_onspace(wordregex.sub(lambda m: wrap_always(m.group(), width), text), width)
+
+
+import math
+
+
+def wrap_always(text, width):
+	"""A simple word-wrap function that wraps text on exactly width characters.
+	   It doesn't split the text in words."""
+	return '\n'.join([text[width * i:width * (i + 1)] \
+	                  for i in xrange(int(math.ceil(1. * len(text) / width)))])
+
+
+if __name__ == '__main__':
+	labels = ('First Name', 'Last Name', 'Age', 'Position')
+	data = \
+		'''John,Smith,24,Software Engineer
+		   Mary,Brohowski,23,Sales Manager
+		   Aristidis,Papageorgopoulos,28,Senior Reseacher'''
+	rows = [row.strip().split(',') for row in data.splitlines()]
+
+	print 'Without wrapping function\n'
+	print indent([labels] + rows, hasHeader=True)
+	# test indent with different wrapping functions
+	width = 10
+	for wrapper in (wrap_always, wrap_onspace, wrap_onspace_strict):
+		print 'Wrapping function: %s(x,width=%d)\n' % (wrapper.__name__, width)
+		print indent([labels] + rows, hasHeader=True, separateRows=True,
+		             prefix='| ', postfix=' |',
+		             wrapfunc=lambda x: wrapper(x, width))
