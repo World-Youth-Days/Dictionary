c2e708e56a7a77f3a8af2ea8e4932a791d5f452f
z-gora <domdlapsa@gmail.com>
2016-05-28 18:08:15 +0200

    No errors when undefined

--- web-interface/query.php
+++ web-interface/query.php
@@ -14,54 +14,57 @@
     } else {
        #echo "Opened database successfully\n";
     }
-    
-    $ids = "SELECT * FROM words WHERE (";
-    if ($_GET['tag'] != ""){
-        $tags = explode(";", $_GET['tag']);
+    if ($_GET['from']!="undefined" && $_GET['to']!="undefined") {
+        $ids = "SELECT * FROM words WHERE (";
+        if ($_GET['tag'] != ""){
+            $tags = explode(";", $_GET['tag']);
 
-        for ($i = 0; $i<count($tags); $i++) {
-            $stmt = $db->prepare("SELECT * FROM '".$tags[$i]."'");
-            $stmt->bindValue(':id', 1, SQLITE3_INTEGER);
-            $result = $stmt->execute();
-            while($row = $result->fetchArray(SQLITE3_ASSOC)) {
-                $ids .= "id=".$row['word_id']." OR ";
+            for ($i = 0; $i<count($tags); $i++) {
+                $stmt = $db->prepare("SELECT * FROM '".$tags[$i]."'");
+                $stmt->bindValue(':id', 1, SQLITE3_INTEGER);
+                $result = $stmt->execute();
+                while($row = $result->fetchArray(SQLITE3_ASSOC)) {
+                    $ids .= "id=".$row['word_id']." OR ";
+                }
             }
+            $ids = substr($ids, 0, -4).") AND (";
+        }
+
+        $stmt = $db->prepare("SELECT * FROM '".$_GET['from']."'");
+        $stmt->bindValue(':id', 1, SQLITE3_INTEGER);
+        $result = $stmt->execute();
+        while($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            $ids .= "id=".$row['word_id']." OR ";
         }
         $ids = substr($ids, 0, -4).") AND (";
-    }
 
-    $stmt = $db->prepare("SELECT * FROM '".$_GET['from']."'");
-    $stmt->bindValue(':id', 1, SQLITE3_INTEGER);
-    $result = $stmt->execute();
-    while($row = $result->fetchArray(SQLITE3_ASSOC)) {
-        $ids .= "id=".$row['word_id']." OR ";
-    }
-    $ids = substr($ids, 0, -4).") AND (";
+        $stmt = $db->prepare("SELECT * FROM '".$_GET['to']."'");
+        $stmt->bindValue(':id', 1, SQLITE3_INTEGER);
+        $result = $stmt->execute();
+        while($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            $ids .= "id=".$row['word_id']." OR ";
+        }
+        $ids = substr($ids, 0, -4).") AND level>=".$_GET['lmin']." AND level<=".$_GET['lmax']." ORDER BY base";
+        #echo $ids;
 
-    $stmt = $db->prepare("SELECT * FROM '".$_GET['to']."'");
-    $stmt->bindValue(':id', 1, SQLITE3_INTEGER);
-    $result = $stmt->execute();
-    while($row = $result->fetchArray(SQLITE3_ASSOC)) {
-        $ids .= "id=".$row['word_id']." OR ";
-    }
-    $ids = substr($ids, 0, -4).") AND level>=".$_GET['lmin']." AND level<=".$_GET['lmax']." ORDER BY base";
-    #echo $ids;
-
-    $result = $db->query($ids);
-    $count = 0;
-    while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-        $count+=1;
-?>
-    <tr>
-        <td class="mdl-data-table__cell--non-numeric base"><?php echo $row['base'] ?></td>
-        <td class="mdl-data-table__cell--non-numeric trans"><?php echo $row['trans'] ?></td>
-        <td class="mdl-data-table__cell--non-numeric mono"><?php echo $row['mono'] ?></td>
-        <td class="mdl-data-table__cell--non-numeric author"><?php echo $row['author'] ?></td>
-        <td class="level"><?php echo $row['level'] ?></td>
-    </tr>
-<?php
-    }
-    if (!$count) {
+        $result = $db->query($ids);
+        $count = 0;
+        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            $count+=1;
+    ?>
+        <tr>
+            <td class="mdl-data-table__cell--non-numeric base"><?php echo $row['base'] ?></td>
+            <td class="mdl-data-table__cell--non-numeric trans"><?php echo $row['trans'] ?></td>
+            <td class="mdl-data-table__cell--non-numeric mono"><?php echo $row['mono'] ?></td>
+            <td class="mdl-data-table__cell--non-numeric author"><?php echo $row['author'] ?></td>
+            <td class="level"><?php echo $row['level'] ?></td>
+        </tr>
+    <?php
+        }
+        if (!$count) {
+            echo "//ABC//";
+        }
+    } else {
         echo "//ABC//";
     }
 ?>
