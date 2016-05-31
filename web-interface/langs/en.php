3fee931e23d888e28889d20356aff2d07f2ba6fa
z-gora <domdlapsa@gmail.com>
2016-06-01 00:39:24 +0200

    Link to contribution

--- web-interface/langs/en.php
+++ web-interface/langs/en.php
@@ -10,7 +10,7 @@ $translations = array(
     "SearchForWordDefLabel" => "Search for word, definition, label...",
     "Languages" => "Languages",
     "Tags" => "Tags",
-    "firstChooseTheLanguages" => "Choose the languages above to get a list of available tags. If a pair of languages is not available, why not create it yourself?",
+    "firstChooseTheLanguages" => "Choose the languages above to get a list of available tags. If a pair of languages is not available, why not <a href='#contribute'>create it yourself?</a>",
     "Level" => "Level",
     "Word" => "Word",
     "Translation" => "Translation",
