32b701005b6c3a076005db306c39204f3bafdeaa
z-gora <domdlapsa@gmail.com>
2016-06-22 01:23:46 +0200

    Tags
    Fixed a huge tags bug which prevented tags with special characters from
    showing.

--- web-interface/tags-language.php
+++ web-interface/tags-language.php
@@ -15,7 +15,7 @@
        #echo "Opened database successfully\n";
     }
     
-    error_reporting(0);
+    //error_reporting(0);
     register_shutdown_function( "fatal_handler" );
     function fatal_handler() {
         $error = error_get_last();
@@ -39,7 +39,7 @@
             $fromIds[] = $row['word_id'];
         }
 //        foreach ($fromIds as $value) {
-//            echo $value;
+//            echo $value.",";
 //        }
 //        echo "<br>";
         #We've got the first (from) condition
@@ -51,27 +51,36 @@
             $toIds[] = $row['word_id'];
         }
 //        foreach ($toIds as $value) {
-//            echo $value;
+//            echo $value.",";
 //        }
 //        echo "<br>";
         
         $stmtLiveTags = "SELECT * FROM tags WHERE flag = 'live'";
+        #echo $stmtLiveTags."<br>";
         $returnLiveTags = $db->query($stmtLiveTags);
         while ($row = $returnLiveTags->fetchArray(SQLITE3_ASSOC)) {
+            #echo $row['tag_name']."<br>";
             #Now for every language combination with the specified FROM we check whether there are any words
             $liveIds = array();
-            $stmtLiveIds = $db->prepare("SELECT * FROM ".$row['tag_name']."");
+            $txtLiveIds = "SELECT * FROM '".$row['tag_name']."'";
+            #echo $txtLiveIds."<br>";
+            $stmtLiveIds = $db->prepare($txtLiveIds);
             $stmtLiveIds->bindValue(':id', 1, SQLITE3_INTEGER);
             $resultLiveIds = $stmtLiveIds->execute();
             while($row2 = $resultLiveIds->fetchArray(SQLITE3_ASSOC)) {
-               $liveIds[] = $row2['word_id'];
+                #echo $row2['word_id']."<br>";
+                $liveIds[] = $row2['word_id'];
             }
             #checking the number of words for language pair
             $count = count(array_intersect($toIds, $fromIds, $liveIds));
+//            foreach($liveIds as $result) {
+//                echo $result, ",";
+//            }
             #echo $count."Count<br>";
             if ($count) {
                 $return .= $row['tag_name'].";";
             }
+            #echo $return."<br>";
         }
         echo $return;
         if ($return == "") echo "//ABC//";
